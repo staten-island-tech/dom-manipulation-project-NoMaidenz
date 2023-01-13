ab392d2cb4ce7a743ecb898e823f3b7ccb490696
NoMaidenz <deathblossom247@gmail.com>
2023-01-13 13:14:11 -0500

    1

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const DOMSelectors = {
   inputDate: document.querySelector("#input-Date"),
   outputContainerDate: document.querySelector("#output-containerDate"),
 };
-
+/* Related to name */
 function createObject() {
   const name = DOMSelectors.inputName.value;
 
@@ -15,7 +15,7 @@ function createObject() {
 
   injectIntoDOM(newObject);
 
-  clearFields();
+  ClearFields();
 }
 
 function injectIntoDOM(object) {
@@ -27,41 +27,75 @@ function injectIntoDOM(object) {
   DOMSelectors.outputContainerNam.appendChild(div);
 }
 
-function clearFields() {
+function ClearFields() {
   DOMSelectors.inputName.value = "";
-  D;
 }
 
 function removeObject() {
   DOMSelectors.outputContainerNam.lastChild.remove();
-
-  function createObject1() {
-    const date = DOMSelectors.inputDate.value;
-
-    const OldObject = {
-      date,
-    };
-
-    injectIntoDOM(OldObject);
-
-    clearFields1();
-  }
-
-  function injectIntoDom(object1) {
-    const div1 = document.createElement("div1");
-    div1.innerHTML = `
-   <p>Date: ${object1.date}</p>
-  `;
-    DOMSelectors.outputContainerDate.appendChild(div1);
-  }
-
-  function clearFields1() {
-    DOMSelectors.inputDate.value = "";
-  }
 }
+
+/* end of name */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+function createObject1() {
+  const date = DOMSelectors.inputDate.value;
+
+  const oldObject = {
+    date,
+  };
+
+  injectIntoDOM(oldObject);
+
+  ClearFields1();
+}
+
+function injectIntoDOM(object1) {
+  const div1 = document.createElement("div1");
+
+  div1.innerHTML = `
+    <p>Date: ${object1.date}</p>
+  `;
+  DOMSelectors.outputContainerDate.appendChild(div1);
+}
+
+function ClearFields1() {
+  DOMSelectors.inputDate.value = "";
+}
+
 function removeObject1() {
   DOMSelectors.outputContainerDate.lastChild.remove();
 }
+/* Start of date */
+/*
+function createObject1() {
+  const date = DOMSelectors.inputDate.value;
+
+  const OldObject = {
+    date,
+  };
+
+  injectIntoDOM(OldObject);
+
+  clearFields1();
+}
+
+function injectIntoDom(object1) {
+  const div1 = document.createElement("div1");
+  div1.innerHTML = `
+   <p>Date: ${object1.date}</p>
+  `;
+  DOMSelectors.outputContainerDate.appendChild(div1);
+}
+
+function clearFields1() {
+  DOMSelectors.inputDate.value = "";
+}
+
+function removeObject1() {
+  DOMSelectors.outputContainerDate.lastChild.remove();
+}
+*/
+/* end of date */
 
 DOMSelectors.form.addEventListener("submit", (event) => {
   event.preventDefault();
